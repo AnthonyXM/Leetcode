97e70f8d91e6fc15cba3f97c966d91752292fad2
AnthonyXM <anthony12590@126.com>
2020-02-19 18:26:18 +0800

    3.finish

--- 3-无重复字符的最长子串/solution.py
+++ 3-无重复字符的最长子串/solution.py
@@ -1,35 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s)==0:
-            return 0
-        i = 0
-        j = 0
-        length = 1
-        while j < len(s) - 1:
-            # 先尝试增加长度，条件：j+1位不与i~j位重复，倒序检索
-            # 若不重复则j++
-            # 若重复，则直接整跳到重复位的后一位并开始滑动
-            k = -1
-            while j < len(s) - 1: # 增加长度的循环,不能继续增加长度时终止
-                for x in range(j, i - 1, -1):
-                    if s[x] == s[j+1]:
-                        k = x
-                        break
-                if k != -1: # 若k不是-1，说明j+1位和k位重复，直接整跳到重复位的后一位并开始滑动
-                    i = k + 1
-                    j = i + length - 1
-                    break
-                else:
-                    j += 1
-                    length += 1
-            # 不清楚当前子串是否存在重复位
-            while j < len(s) - 1: # 滑动的循环，滑动至一个合法位置时终止
-                test = []
-                for x in range(j, i - 1, -1):
-                    if s[x] in test: # x是重复位
-                        i = x + 1
-                        j = i + length - 1
-                        break
-                    else:
-                        test.append(s[x])
-                
+        left = 0 # 从“队列”中pop的下标
+        max_length = 0 # 达到过的最长字串
+        curr_length = 0 # 当前串长度（出入表均需维护）
+        lookup = set() # 查找表
+        for x in range(len(s)):
+             # 每次往后查一个字符
+            while s[x] in lookup: # 直到最后到s[x]的子串无重复元素
+                lookup.remove(s[left])
+                curr_length -= 1
+                left += 1
+            lookup.add(s[x])
+            curr_length += 1
+            if curr_length > max_length: max_length = curr_length
+        return max_length
+
+a = Solution()
+print(a.lengthOfLongestSubstring("pwwkew"))
